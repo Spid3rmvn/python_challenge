682e97182e4cef1752a3b7a43dbde28c0be03015
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-05-14 15:36:59 +0300

    Add ISBN attribute and update Book class representation and details method

--- python_challenge.py
+++ python_challenge.py
@@ -1,17 +1,20 @@
 # Library Management System
 
 class Book:
-    def __init__(self, title, author,genre , publication_year):
+    def __init__(self, title, author,genre , publication_year,isbn):
         self.title = title  # Public as per requirements
         self.author = author  # Public as per requirements
         self.genre = genre
         self.publication_year = publication_year  # Public as per requirements
+        self.isbn = isbn  # Public as per requirements
+    def __repr__(self):
+        return f"Book({self.title}, {self.author}, {self.genre}, {self.publication_year}, {self.isbn})"
     
     def __str__(self):
         return f"{self.title} by {self.author}"
     def get_details(self):
         """Return detailed information about the book."""
-        return f"Title: {self.title}\nAuthor: {self.author}\nGenre: {self.genre}\nPublication Year: {self.publication_year}"
+        return f"Title: {self.title}\nAuthor: {self.author}\nGenre: {self.genre}\nPublication Year: {self.publication_year}\nISBN: {self.isbn}"
         
     def is_recent(self):
         """Check if the book was published in or after 2010."""
